99f8b750f1f4943884327bb8eb31f2b42ae80c0b
Yugo Nishimura <e195719@ie.u-ryukyu.ac.jp>
2019-12-23 22:14:46 +0900

    1回目のコミット

--- src/main/java/jp/ac/uryukyu/ie/e195719/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e195719/Main.java
@@ -4,14 +4,14 @@ package jp.ac.uryukyu.ie.e195719;
  * Created by tnal on 2016/11/13.
  */
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Hero hero = new Hero("勇者", 10, 5);
         Enemy enemy = new Enemy("スライム", 6, 3);
 
         System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
 
         int turn = 0;
-        while(!hero.isDead()  && enemy.isDead() == false ){
+        while (!hero.isDead() && enemy.isDead() == false) {
             turn++;
             System.out.printf("%dターン目開始！\n", turn);
             hero.attack(enemy);
